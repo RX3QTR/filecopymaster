d20b044c00f3c9f8031baa7fbe93797ebd6d508e
RX3QTR <bocharov.yuri@yandex.ru>
2020-08-14 23:30:44 +0300

    add first

---
+++ filecopy.h
@@ -0,0 +1,21 @@
+#ifndef FILECOPY_H
+#define FILECOPY_H
+#include <QString>
+
+//#include <QString>
+
+
+class filecopy
+{
+    QString m_readtempfile, m_getfiles, m_putfiles;
+
+private:
+    void copy(QString strReadTempFile, QString strGetFiles, QString strPutFiles);
+
+public:
+    filecopy(QString readtempfile, QString getfiles, QString putfiles);
+    void fcopy();
+
+};
+
+#endif // FILECOPY_H
---
+++ filedelete.h
@@ -0,0 +1,21 @@
+#ifndef FILEDELETE_H
+#define FILEDELETE_H
+#include <QString>
+
+//#include
+
+
+class filedelete
+{
+    QString m_sMainDir, m_sTempFile;
+
+private:
+    void fdel(QString strMainDir, QString strTempFile);
+
+public:
+    filedelete(QString maindir, QString tempfile);
+    void filedel();
+
+};
+
+#endif // FILEDELETE_H
